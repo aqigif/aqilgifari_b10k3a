16e98e343843d260d57c12fb46ae5444db68f671
Aqil Gifari <50483603+aqigif@users.noreply.github.com>
2019-05-11 20:31:11 +0700

    Add files via upload

--- programmers_6/index.php
+++ programmers_6/index.php
@@ -7,11 +7,20 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+	<style type="text/css">
+		h1 {
+			color: blue;
+		}
+	</style>
     <title>Programmers</title>
   </head>
   <body>
-    <h1>Programmer</h1>
+  	<?php include "koneksi.php"?>
+    <h1>Programmers</h1>
+    <div class="contai">
+    	
+    </div>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
