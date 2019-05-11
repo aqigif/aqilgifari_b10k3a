c81e13ee9c0b51c28ff4cb8c0d62ac0d3d989448
Aqil Gifari <50483603+aqigif@users.noreply.github.com>
2019-05-11 20:17:06 +0700

    Add files via upload

---
+++ 5.php
@@ -0,0 +1,21 @@
+<?php
+
+function($array){
+	for($i = 0; $i < sizeof($array); $i++) {
+		$set = array_multisort(arr$array[$i]);
+	}
+
+}
+
+
+$data = array(
+				array('a','c','b','e','d'),
+				array('g','e','f')
+			);
+
+$datalain = array(
+             	array('g','h','x','j'),
+				array('a','c','b','e','d'),
+				array('q','w','a')
+
+			);
